fbd20cb5de896adf99a4c86867683d7527d768fd
Jordan Center <BigJman1234567@yahoo.com>
2018-02-06 15:59:42 -0600

    header partially done. navbar partially done. picture carousel done exept for picture text.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,188 @@
 
 @section('content')
 
-    {{-- <h1>hello world</h1> --}}
+  <body>
+
+    <main role="main">
+        <div id="carouselExampleControls" class="container carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-100" src="assets/AtkinsFamily.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h2>Family Owned and Operated.</h2>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+              </div>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="assets/Tractor960x480.jpg" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h2>Heading 1</h2>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                {{-- <p><a class="btn btn-lg btn-primary" href="#" role="button">Do Something</a></p> --}}
+              </div>
+            </div>
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="assets/Field960x480.jpg" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h2>Heading 2</h2>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                {{-- <p><a class="btn btn-lg btn-primary" href="#" role="button">Do Something</a></p> --}}
+              </div>
+            </div>
+            </div>
+        </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    
+      {{-- <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="assets/Field.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Example headline.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="assets/Tractor.jpg" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption">
+                <h1>Another example headline.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-right">
+                <h1>One more for good measure.</h1>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div> --}}
+
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      {{-- <div class="container marketing text-center" style="padding-top: 100px;">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Heading</h2>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row --> --}}
+
+
+        <!-- START THE FEATURETTES -->
+        <div class="container" style="padding-top: 100px;">
+        
+            {{-- <div class="row"> --}}
+                {{-- <hr class="featurette-divider"> --}}
+
+                <div class="row featurette" style="padding-bottom: 25px;">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">Heading 1 <span class="text-muted">Sub Heading 1</span></h2>
+                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+                </div>
+                </div>
+
+                <hr class="featurette-divider">
+
+                <div class="row featurette" style="padding-top: 25px; padding-bottom: 25px;">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading">Heading 2 <span class="text-muted">Sub Heading 2</span></h2>
+                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+                </div>
+                </div>
+
+                <hr class="featurette-divider">
+
+                <div class="row featurette" style="padding-top: 25px; padding-bottom: 25px;">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading">Heading 3 <span class="text-muted">Sub Heading 3</span></h2>
+                    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                </div>
+                <div class="col-md-5">
+                    <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+                </div>
+                </div>
+
+                <hr class="featurette-divider">
+            {{-- </div> --}}
+        </div>
+        
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->
+
+
+      <!-- FOOTER -->
+      <footer class="container" style="padding-top: 100px;">
+        <p class="float-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2018 Atkins Seed Service</p>
+      </footer>
+    </main>
+  </body>
+
+
+
 
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,24 +1,94 @@
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<div class="header-div row" style="padding-left: 25px;">
+  <img src="assets/AtkinsSeed-Logo.png" class="m-3 justify-content-start left-align" alt="Atkisns Seed Service" style="max-height: 75px; border-radius: 3px;">
+</div>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+
+<nav class="yellow navbar navbar-expand-sm navbar-light mb-4">
+  <div class="container">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav nav-fill w-100">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/Precision-Planting">Precision Planting</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/Pioneer">Pioneer</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/Pioneer">Seed Treatment</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/Crop-Insurance">Crop Insurance</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/Crop-Insurance">Custom Farming</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/Contact-Us">Contact Us</a>
+        </li>
+        {{-- <li class="nav-item active dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown01">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li> --}}
+      </ul>
+    </div>
   </div>
 </nav>
+
+{{-- <div class="container">
+  <div class="row">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+        <a href="/">
+          <img class="logo" src="assets/AtkinsSeed-Logo.png">
+        </a>
+      <!-- <a class="navbar-brand" href="#">Fixed navbar</a> -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="d-flex collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/Precision-Planting">Precision Planting</a>
+          </li>
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/Pioneer">Pioneer</a>
+          </li>
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/Pioneer">Seed Treatment</a>
+          </li>
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/Crop-Insurance">Crop Insurance</a>
+          </li>
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/Crop-Insurance">Custom Farming</a>
+          </li>
+          <li class="p-2 nav-item active">
+            <a class="nav-link" href="/Contact-Us">Contact Us</a>
+          </li>
+          <li class="p-2 nav-item active dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</div> --}}
